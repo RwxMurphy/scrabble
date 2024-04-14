a0b0b9b41fd136f038b7fcc3add4a3f03d0202fe
amurfie2003 <amurfie2003@gmail.com>
2024-04-14 16:32:12 -0400

    Create score_word function to calculate value of a word

--- scrabble.py
+++ scrabble.py
@@ -1,11 +1,30 @@
 letters = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    ]
+]
 points = [
-    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 
+    1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
     4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
-    ]
+]
 
 letters_points_zipped = zip(letters, points)
-letter_to_points = {key:value for key, value in letters_points_zipped}
+letter_to_points = {key: value for key, value in letters_points_zipped}
+
+# Add blank space key value pair to the dictionary
+letter_to_points[" "] = 0
+
+# Create function score_word that takes in a word and returns its point value.
+
+
+def score_word(word):
+    score = 0
+    word_to_upper = word.upper()
+
+    for c in word_to_upper:
+        score += letter_to_points[c]
+    return score
+
+
+# function test
+score = score_word("test")
+print(f"The score of 'test' is: {score}")
